e31f1261fdc656c3ce641c1cb4478a2e9bf04ebb
oskar <oskarje.ontc@gmail.com>
2014-02-25 22:21:28 +0100

    jechane

--- _plugins/dox_to_markdown.rb
+++ _plugins/dox_to_markdown.rb
@@ -7,8 +7,8 @@ module Jekyll
       site.pages.each do |page|
         if page.ext=='.dox'
           page.ext='.markdown'
-          page.content='{{ 1 | show_as_named_link | upcase}}'
         end
+
       end
     end
   end
--- _plugins/show_as_named_link.rb
+++ _plugins/show_as_named_link.rb
@@ -1,13 +1,23 @@
 module Jekyll
   module ShowAsNamedLink
     def show_as_named_link(input)
+      @DoxUrl = ''
+      @DoxName = ''
       # get dox document as proper class
-      dox = DoxReader.get_dox_by_id(input)
+      #dox = DoxReader.get_dox_by_id(input)
       # write and return html code
+
+    @context.registers[:site].pages.each do |page|
+        if page.data['DoxID'] == input
+          @DoxUrl = page.data['DoxURL']
+          @DoxName = page.data['DoxName']
+        end
+      end
+
       %|
 <div id="question-header">
   <h1 itemprop="name">
-    <a href="#{dox.url}" class="question-hyperlink">#{dox.name}</a>
+    <a href="#{@DoxUrl}" class="question-hyperlink">#{@DoxName}</a>
   </h1>
 </div>|
     end
