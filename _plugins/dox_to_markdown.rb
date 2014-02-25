464b1866938448070c159e9b1e711410b0bf2ec5
sliperro <sliperro@gmail.com>
2014-02-25 20:36:12 +0100

    Generator dox na markdown

---
+++ _plugins/dox_to_markdown.rb
@@ -0,0 +1,15 @@
+module Jekyll
+
+  class DoxToMarkdown < Generator
+    safe true
+
+    def generate(site)
+      site.pages.each do |page|
+        if page.ext=='.dox'
+          page.ext='.markdown'
+        end
+      end
+    end
+  end
+
+end
